9c450ed530a0fa0bae023329619a8b95d10af4db
tpope0928 <tpope0928@gmail.com>
2020-05-27 21:49:58 -0400

    model relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -18,6 +19,6 @@ Game.create(user_id: 2, score: 600, accuracy: 65, headshot_percent: 50)
 Game.create(user_id: 2, score: 700, accuracy: 69, headshot_percent: 59)
 Game.create(user_id: 2, score: 850, accuracy: 74, headshot_percent: 72)
 
-Game.create(user_id: 1, score: 500, accuracy: 40, headshot_percent: 30)
-Game.create(user_id: 1, score: 550, accuracy: 50, headshot_percent: 40)
-Game.create(user_id: 1, score: 650, accuracy: 60, headshot_percent: 50)
+Game.create(user_id: 3, score: 500, accuracy: 40, headshot_percent: 30)
+Game.create(user_id: 3, score: 550, accuracy: 50, headshot_percent: 40)
+Game.create(user_id: 3, score: 650, accuracy: 60, headshot_percent: 50)
