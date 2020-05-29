07ce94e11985df2214c6825f8cf6a84c7930de10
tpope0928 <tpope0928@gmail.com>
2020-05-29 12:19:39 -0400

    game controller done

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::GamesController < ApplicationController
+  def index
+    games = Game.all
+    render json: games
+  end
+
+  def create
+    game = Game.new(game_params)
+    if game.save
+      render json: game, status: :accepted
+    else
+      render json: {errors: game.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:score, :user_id, :accuracy, :headshot_percent)
+  end
 end
