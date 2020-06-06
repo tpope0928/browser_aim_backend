0fccf2d146708845cf8251dc305e858b3cb60719
tpope0928 <tpope0928@gmail.com>
2020-06-06 19:14:55 -0400

    user controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    @user = User.find(user_params[:id])
+    render json: @user
+  end
+
+  def create
+    @user = User.find_or_create_by(username: user_params[:username])
+    render json: @user
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :username)
+  end
+end
